5086b9a32dcdd7ef68dd5a41fb7331acde6e7883
Ross Hadden <rosshadden@gmail.com>
2017-02-09 00:22:11 -0500

    Embedded SpiderMonkey.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#[macro_use]
+extern crate js;
+
+use js::jsapi::CompartmentOptions;
+use js::jsapi::JS_NewGlobalObject;
+use js::jsapi::OnNewGlobalHookOption;
+use js::jsval::UndefinedValue;
+use js::rust::{Runtime, SIMPLE_GLOBAL_CLASS};
+
+use std::ptr;
+
+fn evaluate() {
+	let rt = Runtime::new();
+	let cx = rt.cx();
+
+	unsafe {
+		rooted!(in(cx) let global =
+			JS_NewGlobalObject(
+				cx, &SIMPLE_GLOBAL_CLASS, ptr::null_mut(),
+				OnNewGlobalHookOption::FireOnNewGlobalHook,
+				&CompartmentOptions::default()
+			)
+		);
+		rooted!(in(cx) let mut rval = UndefinedValue());
+		let _ = rt.evaluate_script(global.handle(), "const foo = () => 5; let bar = foo(); bar;", "test", 1, rval.handle_mut());
+		println!("{}", rval.is_number());
+	}
+}
+
 fn main() {
 	println!("Hi, Mom!");
+	evaluate();
 }
