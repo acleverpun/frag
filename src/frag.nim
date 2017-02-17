ed18696e2ee2c55aefe7c9ff8fbcc7c48806749f
Ross Hadden <rosshadden@gmail.com>
2017-02-17 17:19:29 -0500

    Removed app in favor of game.

--- src/frag.nim
+++ src/frag.nim
@@ -7,24 +7,22 @@ const
   windowFlags = 0
   rendererFlags = sdl.RendererAccelerated or sdl.RendererPresentVsync
 
-type
-  App = ref AppObj
-  AppObj = object
-    window*: sdl.Window
-    renderer*: sdl.Renderer
-
 type Game* = ref object of RootObj
-  app: App
-  isRunning: bool
+  window*: sdl.Window
+  renderer*: sdl.Renderer
+
+method init(this: Game) {.base.} = discard
+method update(this: Game) {.base.} = discard
+method render(this: Game) {.base.} = discard
 
 type SdlEx = object of Exception
 template sdlFailIf(cond: typed, err: string) =
   if cond: raise SdlEx.newException(err & ", [SDL] error: " & $sdl.getError())
 
-proc init(app: App): bool =
+proc setupSdl(this: Game): bool =
   sdlFailIf sdl.init(sdl.InitVideo) != 0: "Failed to initialize SDL"
 
-  app.window = sdl.createWindow(
+  this.window = sdl.createWindow(
     title,
     sdl.WindowPosUndefined,
     sdl.WindowPosUndefined,
@@ -32,49 +30,42 @@ proc init(app: App): bool =
     height,
     windowFlags
   )
-  sdlFailIf app.window == nil: "Failed to create window"
+  sdlFailIf this.window == nil: "Failed to create window"
 
-  app.renderer = sdl.createRenderer(app.window, -1, rendererFlags)
-  sdlFailIf app.renderer == nil: "Failed to create renderer"
+  this.renderer = sdl.createRenderer(this.window, -1, rendererFlags)
+  sdlFailIf this.renderer == nil: "Failed to create renderer"
 
   return true
 
-proc exit*(game: Game) =
-  game.app.renderer.destroyRenderer()
-  game.app.window.destroyWindow()
+proc exit*(this: Game) =
+  this.renderer.destroyRenderer()
+  this.window.destroyWindow()
   sdl.quit()
   system.quit()
 
 proc run*[Game]() =
-  var game: Game = Game(app: nil)
-  var app = App(window: nil, renderer: nil)
-  game.app = app
-  game.isRunning = true
+  var game: Game = Game(window: nil, renderer: nil)
 
-  if init(app):
-    when compiles(game.init): game.init()
+  if game.setupSdl():
+    game.init()
 
-    while game.isRunning == true:
-      when compiles(game.update): game.update()
+    while true:
+      game.update()
 
-      sdlFailIf app.renderer.setRenderDrawColor(0x00, 0x00, 0x00, 0xff) != 0: "Failed to set draw color"
-      sdlFailIf app.renderer.renderClear() != 0: "Failed to clear screen"
+      sdlFailIf game.renderer.setRenderDrawColor(0x00, 0x00, 0x00, 0xff) != 0: "Failed to set draw color"
+      sdlFailIf game.renderer.renderClear() != 0: "Failed to clear screen"
 
-      when compiles(game.render): game.render()
-      app.renderer.renderPresent()
+      game.render()
+      game.renderer.renderPresent()
 
   game.exit()
 
 when isMainModule:
   type MyGame = ref object of Game
 
-  proc init(this: MyGame) = discard
-
   var i = 0
-  proc update(this: MyGame) =
+  method update(this: MyGame) =
     inc(i)
     if i == 50: this.exit()
-
-  proc render(this: MyGame) = discard
 
   run[MyGame]()
