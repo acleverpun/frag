c0ce728fe15e340170aeea2739fcbe92d63476a1
Ross Hadden <rosshadden@gmail.com>
2017-02-20 19:25:56 -0500

    Broke out color module.

--- lib/frag/draw.nim
+++ lib/frag/draw.nim
@@ -1,35 +1,10 @@
-import basic2d
-import game
-import sdl2/sdl
 from colors import nil
+import basic2d
+import draw/color
+import game
+import sdl2/sdl except Color
 
-export Color
-
-type rgb* = tuple[ r, g, b: uint8 ]
-type rgbi* = tuple[ r, g, b: int ]
-type rgba* = tuple[ r, g, b, a: uint8 ]
-type rgbai* = tuple[ r, g, b, a: int ]
-
-# handle channels
-converter toUint8(val: int): uint8 = val.uint8
-proc color*(r, g, b: uint8 = 0, a: uint8 = 0xff): Color = Color(r: r, g: g, b: b, a: a)
-
-# handle tuples
-proc color*(val: rgb): Color = color(val.r, val.g, val.b)
-proc color*(val: rgbi): Color = color(val.r, val.g, val.b)
-proc color*(val: rgba): Color = color(val.r, val.g, val.b, val.a)
-proc color*(val: rgbai): Color = color(val.r, val.g, val.b, val.a)
-
-# handle colors module
-proc color*(val: colors.Color): Color = color(colors.extractRGB(val))
-proc color*(val: string): Color = color(colors.parseColor(val))
-proc color*(val: int): Color = color(colors.Color(val))
-
-converter toColor*(val: string): Color = color(val)
-converter toColor*(val: rgb): Color = color(val)
-converter toColor*(val: rgbi): Color = color(val)
-converter toColor*(val: rgba): Color = color(val)
-converter toColor*(val: rgbai): Color = color(val)
+export color
 
 proc drawPoint*(game: Game, pos: Vector2d, color: Color) =
   discard game.renderer.setRenderDrawColor(color)
