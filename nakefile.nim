a0818d7aca20d82df11564d7aad0be120d42b28a
Ross Hadden <rosshadden@gmail.com>
2017-03-18 10:28:40 -0400

    Improved nakefile.

--- nakefile.nim
+++ nakefile.nim
@@ -1,15 +1,26 @@
-import nake, strutils
+import algorithm, nake, os, strutils
 
 const
   fragBin = "lib/frag"
   exDir = "examples"
   exBin = "main"
 
-proc compile(bin: string) = shell(nimExe, "c", "-d:debug", bin)
-proc run(bin: string) = shell(nimExe, "c", "-r", "-d:debug", bin)
-proc runEx(name: string) = run(join(@[ exDir, name, exBin ], "/"))
+proc compile(bin: string) = direShell(nimExe, "c", bin)
+proc run(bin: string) = direShell(nimExe, "c", "-r", bin)
+proc runExample(name: string) = run(join(@[ exDir, name, exBin ], "/"))
 
-# TODO: doesn't work
-task defaultTask, "Compile FRAG": compile(fragBin)
+proc registerExample(path: string) =
+  var parts = path.split('-')
+  let id = parts[0]
+  parts.delete(0)
+  task id, "run example " & parts.join("-"):
+    runExample(path)
 
-task "gnop", "Run gnop example.": runEx("gnop")
+var examples: seq[string] = @[]
+for kind, path in walkDir("examples", true):
+  if path.contains("assets"): continue
+  examples.add(path)
+sort(examples, cmp[string])
+
+for path in examples:
+  registerExample(path)
